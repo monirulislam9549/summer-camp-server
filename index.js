a68eca1c1bc13b8a3724f0c41935531e4616b3a5
monirulislam9549 <monirulislam9549@gmail.com>
2023-06-11 13:03:00 +0600

    delete api done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.skg6sgn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -75,6 +75,13 @@ async function run() {
     app.post("/selects", async (req, res) => {
       const item = req.body;
       const result = await selectsCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/selects/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectsCollection.deleteOne(query);
       res.send(result);
     });
 
