f9e922b5b06cc4b7c5fce79ce6a0afb2e1ec6b13
monirulislam9549 <monirulislam9549@gmail.com>
2023-06-12 23:11:29 +0600

    last commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -239,6 +240,19 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await selectsCollection.deleteOne(query);
       res.send(result);
+    });
+
+    // payment
+
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
     });
 
     // Send a ping to confirm a successful connection
